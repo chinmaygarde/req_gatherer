b5e13906c4457c7efa8768ce2f0d6a7d6bf7b37f
Chinmay Garde <chinmaygarde@gmail.com>
2010-08-31 16:36:27 -0400

    Removed validates_presence_of for a couple of requirement fields

--- app/models/requirement.rb
+++ app/models/requirement.rb
@@ -3,5 +3,5 @@ class Requirement < ActiveRecord::Base
   belongs_to :auth_level
   belongs_to :status
   
-  validates_presence_of :title, :description, :preconditions, :postconditions, :steps_to_use_case
+  validates_presence_of :title, :description
 end
